fd5774b144727d47dc3a36e2fb777c7dd172569e
Viktoriia <viktoriia.dvornyk@ihsmarkit.com>
2023-06-04 22:35:53 +0300

    add comments

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ import model_inference as mi
 import cv2
 
 class App:
+    # initialize application
     def __init__(self, root):
         self.root = root
         self.root.title("Ship Detector")
@@ -13,7 +14,7 @@ class App:
 
         self.model = mi.load_ml_model()
         self.canvas = None
-
+        # add label and button
         load_label = tk.Label(self.root, text="Load Ship Image:")
         load_label.pack()
 
@@ -23,13 +24,13 @@ class App:
     def load_file(self):
         if self.canvas:
             self.canvas.get_tk_widget().destroy()
-
+        # load file
         file_path = filedialog.askopenfilename(filetypes=[("JPG Files", "*.jpg")])
-
+        # refer to inference
         image, resized_image = mi.preprocessing(file_path)
         predicted_masks = mi.find_segment(self.model, [image])
         ship_detected_result = mi.detect_ship(predicted_masks.squeeze())
-
+        # display graph
         fig = plt.figure()
         plt.subplot(1, 2, 1)
         plt.imshow(cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB))
@@ -43,7 +44,7 @@ class App:
         self.canvas.draw()
         self.canvas.get_tk_widget().pack()
 
-
+# start an app
 root = tk.Tk()
 app = App(root)
 root.mainloop()
